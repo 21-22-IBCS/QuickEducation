221ce332076761b349e7f2da00e6b79251e4f692
Gwynyn <gwynyn_hayes@aw.org>
2021-09-22 14:36:43 -0700

    Python Basics

---
+++ pythonBasics.py
@@ -0,0 +1,19 @@
+def main():
+# part 2
+    print("Python Basic Commands Practice")
+    print("September 22, 2021")
+# part 3
+    y = 17
+    x = 23
+    print(x + y)
+# part 4
+    print(x * y)
+# part 5
+    USGstudents = 20
+    print("There are currently " + str(USGstudents) + " students in the Annie Wright Uppers School for Girls")
+    print("There are currently " + str(USGstudents + 60) + " students in the Annie Wright Uppers School for Girls")
+    print("There are currently " + str(USGstudents + 137) + " students in the Annie Wright Uppers School for Girls")
+# part 6    
+    #refer to parts 1-5
+if __name__ == "__main__":
+    main()
